6547ea726c8cf72159761a44ee8e1495fe270e2d
Tilak Naik <jalavalli321@gmail.com>
2025-09-20 23:01:05 +0530

    python

---
+++ operators.py
@@ -0,0 +1,38 @@
+#opertor
+
+
+
+num=int(input("enter the number:"))
+num1=int(input("enter the number:"))
+
+##+
+addi=int(num+num1)
+print("addition is",addi)
+
+##-
+addi1=int(num-num1)
+print("substraction is",addi1)
+
+##*
+
+addi2=int(num*num1)
+print("multiplication is",addi2)
+
+##/
+
+addi3=int(num/num1)
+print("division is",addi3)
+
+##% module operator mean reminder kodutte
+
+addi4=int(num%num1)
+print("reminder is:",addi4)
+
+#  // divide a by b and give only the integer part (floor)
+
+add5=int(num//num1)
+print("division is",add5)
+
+# ** a raised to the power of b
+add6=int(num**num1)
+print(num, "the power of",num1,"is" ,add6)
